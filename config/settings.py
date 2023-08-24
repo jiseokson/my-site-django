95b4ca7d6e0fb7ecf04d1f7df80a323e18c6b4c9
Jiseok Son <jiseokson@gmail.com>
2023-08-24 15:32:37 +0900

    add: CORS 정책

--- config/settings.py
+++ config/settings.py
@@ -45,10 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'posts',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +141,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = [
+    'http://www.igoofficial.com',
+    'http://igoofficial.com',
+    'http://localhost:3000',
+]
