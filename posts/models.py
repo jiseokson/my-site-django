b5ef7a2ef5c40c60a3218376fc407c9648e5c035
Jiseok Son <jiseokson@gmail.com>
2023-08-25 15:12:56 +0900

    add: post model엣 image thumbnail 추가

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from ..config.settings import STATIC_ROOT
 
 class Post(models.Model):
     title = models.CharField(max_length=256)
     content = models.TextField()
+
+    thumbnail = models.ImageField(upload_to=STATIC_ROOT, default=(STATIC_ROOT / 'default.png'))
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
