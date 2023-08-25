9928c396561873e7aac6d7650688d7ca656013ec
Jiseok Son <jiseokson@gmail.com>
2023-08-25 15:16:13 +0900

    fix: static root

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=256)
     content = models.TextField()
 
-    thumbnail = models.ImageField(upload_to=STATIC_ROOT, default=(STATIC_ROOT / 'default.png'))
+    thumbnail = models.ImageField(upload_to='home/ubuntu/static', default='home/ubuntu/static/default.png')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
